6ec5188ed7aab6479f4b1235567c08f759e366cb
Felipe Pagani <feh.pagani@gmail.com>
2021-04-06 17:36:57 -0300

    project created

---
+++ src/test/java/com/treinamentoapi/workshopspringbootmongodb/WorkshopSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamentoapi.workshopspringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
