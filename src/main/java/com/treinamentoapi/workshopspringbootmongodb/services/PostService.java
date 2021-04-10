dc0176e89c42337c4e34f3f01472606ddb91f680
Felipe Pagani <feh.pagani@gmail.com>
2021-04-10 14:27:22 -0300

    Getting a post by id

--- src/main/java/com/treinamentoapi/workshopspringbootmongodb/services/PostService.java
+++ src/main/java/com/treinamentoapi/workshopspringbootmongodb/services/PostService.java
@@ -1,19 +1,23 @@
-//package com.treinamentoapi.workshopspringbootmongodb.services;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import com.treinamentoapi.workshopspringbootmongodb.repository.PostRepository;
-//
-//@Service
-//public class PostService {
-//	
-//	@Autowired
-//	private PostRepository postRepository;
-//	
-//	
-//	
-//	
-//	
-//	
-//}
+package com.treinamentoapi.workshopspringbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.treinamentoapi.workshopspringbootmongodb.domain.Post;
+import com.treinamentoapi.workshopspringbootmongodb.repository.PostRepository;
+import com.treinamentoapi.workshopspringbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
